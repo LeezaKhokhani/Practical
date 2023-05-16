5c3c65cc7fe813059e31cf725b85ee7cb12eaa40
khokhanileeza@gmai.com <leeza6234>
2023-05-16 12:28:13 +0530

    Project

---
+++ lib/main.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+import 'package:practical/screens/homepage.dart';
+
+void main() {
+  runApp(
+    const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: HomePage(),
+    ),
+  );
+}
